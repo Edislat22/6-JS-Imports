95e649934dc1d57f20d470f12e85afc0fa5c3ce9
Eduardas <latoanas@gmail.com>
2021-02-04 13:38:08 +0200

    People

--- js/components/labas.js
+++ js/components/labas.js
@@ -2,7 +2,16 @@ function labas(data) {
    for (let i = 0; i < data.length; i++) {
       const person = data[i];
 
-      console.log(person);
+      //console.log(person);
+
+      //console.log(person['name']);
+      //console.log(person['fruit']);
+    
+   //rekomendacija naudoti toki varianta:
+      //console.log(person.name);
+      //console.log(person.fruit);
+
+      console.log(`hi, my name is ${person.name} and I like ${person.fruit}`);
    }
 }
 
---
+++ js/data/people.js
@@ -0,0 +1,20 @@
+const people = [
+   {
+      name: 'Onute',
+      fruit: 'apple'
+   },
+   {
+      name: 'Petras',
+      fruit: 'potato'
+   },
+   {
+      name: 'Maryte',
+      fruit: 'banana'
+   },
+   {
+      name: 'Jonukas',
+      fruit: 'cherri'
+   }   
+];
+
+export { people }
